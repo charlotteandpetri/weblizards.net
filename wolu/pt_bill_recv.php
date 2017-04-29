1d1cb5f4262407d2e613329f3453b6629513aa2e
Petri Laihonen <pietu@charlotteandpetri.net>
2017-04-29 19:17:28 +0300

    Removed a lot of crud

--- wolu/pt_bill_recv.php
+++
@@ -1,7 +0,0 @@
-<?php
-while(list($var,$val) = each($_REQUEST)) {
-	$display .= "$var = $val <br/>\n";
-}
-$display = urldecode(str_replace("\n","<br/>",$display));
-echo "Incoming Values : $display";
-?>
